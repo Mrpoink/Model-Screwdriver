891ceee2fcc0b5865d633a13f2ed14b05dadfc14
Brandon Dean <101386937+Mrpoink@users.noreply.github.com>
2026-03-31 13:32:09 -0400

    Added Kmeans clustering and silhouette scores

--- DataExtraction/GetModelWeights.py
+++ DataExtraction/GetModelWeights.py
@@ -1,4 +1,7 @@
 import torch
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 
 class TaskVectorHarvester:
     
@@ -9,8 +12,13 @@ class TaskVectorHarvester:
         self.tokenizer = tokenizer
         self.device = device
         
+        ## Extraction cache
         self.x_in_cache = []
         self.y_out_cache = []
+        
+        ## K-Means Cluster Cache
+        self.cluster_activations = {}
+        self.cluster_hooks = []
         
     def _get_target_module(self, model, layer_idx : int):
         """Currently configured for the BERT models
@@ -37,12 +45,81 @@ class TaskVectorHarvester:
         
         
         ## Average across the sequence to get a single vector per prompt
-        ## WARNING: THIS CAN CAUSE DATA LOSS, WE MAY NEED A BETTER WAY TO DO THIS
-        x_mean = x_raw.mean(dim=1).detach()
-        y_mean = y_raw.mean(dim=1).detach()
+        ## WARNING: AVERAGING CAN CAUSE DATA LOSS, WE MAY NEED A BETTER WAY TO DO THIS
+        # x_mean = x_raw.mean(dim=1).detach()
+        # y_mean = y_raw.mean(dim=1).detach()
+        ## SINCE AVERAGING REMOVES DATA BUT WE ARE CURRENTLY JUST USING A BERT MODEL
+        x_mean = x_raw[:, 0, :].detach()
+        y_mean = y_raw[:, 0, :].detach()
         
         self.x_in_cache.append(x_mean)
         self.y_out_cache.append(y_mean)
+        
+    def _hook_factory(self, layer_idx:int):
+        """Generates layer-specific hook
+
+        Args:
+            layer_idx (int): layer idx for model
+        """
+        
+        def _hook(module, input_tensor, output_tensor):
+            
+            y_raw = output_tensor[0] if isinstance(output_tensor, tuple) else output_tensor
+            ## THIS IS FOR BERT MODELS ONLY
+            ## CHANGE THIS IF YOU CHANGE THE MODEL
+            y_cls = y_raw[:, 0, :].detach().cpu().numpy()
+            self.cluster_activations[layer_idx].append(y_cls)
+            
+        return _hook
+    
+    def find_optimal_layer(self, model, base_prompts:list, task_promtps:list) -> int:
+        """_summary_
+
+        Args:
+            model (_type_): model
+            base_prompts (list): neutral prompts list
+            task_promtps (list): task-specific prompts list
+
+        Returns:
+            int: layer_idx for model given prompts
+        """
+        
+        num_layers = model.config.num_hidden_layers
+        self.cluster_activations = {i: [] for i in range(num_layers)}
+        
+        ## Attach wiretaps to ALL layers
+        for i in range(num_layers):
+            module = self._get_target_module(model, i)
+            self.cluster_hooks.append(module.register_forward_hook(self._hook_factory(i)))
+            
+        ## Run Inference to stimulate heads
+        with torch.no_grad():
+            for prompts in [base_prompts, task_promtps]:
+                for text in prompts:
+                    inputs = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True).to(self.device)
+                    model(**inputs)
+                    
+        ## Remove wiretaps
+        for hook in self.cluster_hooks:
+            hook.remove()
+        
+        self.cluster_hooks = []
+        
+        ## Now, we must compare the heads to themselves, so let's implement Silhouette Scores
+        best_layer, best_score = 0, -1.0
+        true_labels = np.array([0] * len(base_prompts) + [1] * len(task_promtps))
+        
+        for i in range(num_layers):
+            layer_data = np.vstack(self.cluster_activations[i])
+            kmeans = KMeans(n_clusters=num_layers, random_state=42, n_init=10)
+            kmeans.fit(layer_data)
+            score = silhouette_score(layer_data, true_labels)
+            
+            if score > best_score:
+                best_score = score
+                best_layer = i
+                
+        return best_layer
         
         
     def extract_task_matrices(self, model, layer_idx:int, base_prompts: list, task_prompts: list):
@@ -80,6 +157,7 @@ class TaskVectorHarvester:
         
         hook_handle.remove()
         
+        ## Forge the LoRA matrices
         v_task = y_task_out - y_base_out
         norm_sq = torch.dot(x_task_in, x_task_in) + 1e-8
         
